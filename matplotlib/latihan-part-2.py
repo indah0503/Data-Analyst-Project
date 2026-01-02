02c04eb1eadbfe0d5777beb6d390b311590ddd8a
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-02 23:25:59 +0700

    Update latihan-part-2.py

--- matplotlib/latihan-part-2.py
+++ matplotlib/latihan-part-2.py
@@ -29,6 +29,43 @@ plt.show()
 ''' Kustomisasi Colormap '''
 plt.clf()
 dataset.groupby(['order_month','province'])['gmv'].sum().unstack().plot(cmap='Set1')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province',loc='center',pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), shadow=True, ncol=3, title='Province', fontsize=9, title_fontsize=11)
+plt.gcf().set_size_inches(10, 5)
+plt.tight_layout()
+plt.show()
+
+''' Membuat Line Chart GMV Breakdown by Top Provinces '''
+# Berhubung kita fokusnya ke provinsi-provinsi yang GMV-nya besar,
+# kalau begitu kita highlight untuk top 5 provinsi saja, yang lainnya kita gabungkan menjadi ‘other’.
+# Buat variabel untuk 5 propinsi dengan GMV tertinggi
+top_provinces = (dataset.groupby('province')['gmv']
+                        .sum()
+                        .reset_index()
+                        .sort_values(by='gmv',ascending=False)
+                        .head(5))
+print(top_provinces)
+# Buat satu kolom lagi di dataset dengan nama province_top
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+# Plot multi-line chartnya
+import matplotlib.pyplot as plt
+dataset.groupby(['order_month','province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province', loc='center', pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
 plt.show()
 
 
