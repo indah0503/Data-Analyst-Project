441ff650fa980da0de8b7f77827584d70f25e011
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-03 17:55:26 +0700

    Update latihan-part-2.py

--- matplotlib/latihan-part-2.py
+++ matplotlib/latihan-part-2.py
@@ -170,6 +170,6 @@ plt.figure()
 plt.hist(data_per_customer['orders'], range=(1,5))
 plt.title('Distribution of Number of Orders per Customer\nDKI Jakarta in Q4 2019',fontsize=15, color='blue')
 plt.xlabel('Number of Orders', fontsize=12)
-plt.xlabel('Number of Customers', fontsize=12)
+plt.ylabel('Number of Customers', fontsize=12)
 plt.show()
 
