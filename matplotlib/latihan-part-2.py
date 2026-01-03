10d9d9d95f6bbc719a60888d5dc576a51329eb11
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-03 19:10:06 +0700

    Update latihan-part-2.py

--- matplotlib/latihan-part-2.py
+++ matplotlib/latihan-part-2.py
@@ -193,3 +193,22 @@ labels, locations = plt.xticks()
 plt.xticks(labels, (labels/1000000).astype(int))
 plt.show()
 
+''' Membuat Scatterplot '''
+plt.clf()
+# Scatterplot pertama
+plt.figure()
+plt.scatter(data_per_customer['quantity'], data_per_customer['gmv'])
+plt.show()
+# Hasil grafiknya banyak terkumpul di nilai-nilai kecil, sehingga tidak begitu terlihat detailnya di area situ.
+# Untuk itu ditambahkan parameter lain mirip seperti pada Histogram di atas.
+# Scatterplot kedua: perbaikan scatterplot pertama
+plt.figure(figsize=(10,8))
+plt.scatter(data_per_customer['quantity'], data_per_customer['gmv'], marker='+', color='green')
+plt.title('Correlation of Quantity and GMV per Customer\nDKI Jakarta in Q4 2019',fontsize=15, color='blue')
+plt.xlabel('Quantity', fontsize=12)
+plt.ylabel('GMV (in Millions)', fontsize=12)
+plt.xlim(xmin=0,xmax=300)
+plt.ylim(ymin=0,ymax=150000000)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000).astype(int))
+plt.show()
