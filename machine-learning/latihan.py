a0f26ed5bcb6c5163a2681c06f5cb03ae38db29c
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-04 19:48:02 +0700

    Update latihan.py

--- machine-learning/latihan.py
+++ machine-learning/latihan.py
@@ -31,3 +31,80 @@ plt.title('Purchase on Weekends', fontsize = 20)
 plt.xlabel('Weekend or not', fontsize = 14)
 plt.ylabel('count', fontsize = 14)
 plt.show()
+
+''' Tugas Praktek '''
+# Membuat visualisasi berupa histogram yang menggambarkan jumlah customer untuk setiap Region.
+# visualizing the distribution of customers around the Region
+plt.hist(dataset['Region'], color = 'lightblue')
+plt.title('Distribution of Customer', fontsize = 20)
+plt.xlabel('Region Codes', fontsize = 14)
+plt.ylabel('Count Users', fontsize = 14)
+plt.show()
+
+''' Data Pre-processing: Handling Missing Value - Part 1 '''
+#checking missing value for each feature  
+print('Checking missing value for each feature:')
+print(dataset.isnull().sum())
+#Counting total missing value
+print('\nCounting total missing value:')
+print(dataset.isnull().sum().sum())
+
+''' Data Pre-processing: Handling Missing Value - Part 2 '''
+# Metode ini dapat diterapkan jika tidak banyak missing value dalam data, sehingga walaupun data point ini dihapus, kita masih memiliki sejumlah data yang cukup untuk melatih model Machine Learning.
+# Tetapi jika kita memiliki banyak missing value dan tersebar di setiap variabel, maka metode menghapus missing value tidak dapat digunakan.
+# Kita akan kehilangan sejumlah data yang tentunya mempengaruhi performansi model.
+#Drop rows with missing value   
+dataset_clean = dataset.dropna()  
+print('Ukuran dataset_clean:', dataset_clean.shape) 
+
+''' Data Pre-processing: Handling Missing Value - Part 3 '''
+# Menggunakan metode impute missing value, yaitu mengisi record yang hilang ini dengan suatu nilai.
+# Ada berbagai teknik dalam metode imputing, mengisi missing value dengan nilai mean, median, modus, atau nilai konstan, sampai menggunakan nilai yang diestimasi oleh suatu predictive model.
+# Untuk kasus ini, kita akan menggunakan imputing sederhana yaitu menggunakan nilai rataan atau mean.
+print("\nAfter imputation:")
+# Fill missing value with mean of feature value  
+dataset.fillna(dataset.mean(), inplace = True)
+# Checking missing value for each feature  
+print(dataset.isnull().sum())
+# Counting total missing value  
+print(dataset.isnull().sum().sum())
+
+''' Tugas Praktek '''
+# Praktekkan metode imputing missing value dengan menggunakan nilai median.
+print("\nAfter imputation:")
+# Fill missing value with median of feature value  
+dataset.fillna(dataset.median(), inplace = True)
+# Checking missing value for each feature  
+print(dataset.isnull().sum())
+# Counting total missing value  
+print(dataset.isnull().sum().sum())
+
+''' Data Preprocessing: Scaling '''
+from sklearn.preprocessing import MinMaxScaler  
+#Define MinMaxScaler as scaler  
+scaler = MinMaxScaler()  
+#list all the feature that need to be scaled
+scaling_column = ['Administrative', 'Administrative_Duration', 'Informational', 'Informational_Duration', 'ProductRelated', 'ProductRelated_Duration', 'BounceRates', 'ExitRates', 'PageValues']
+#Apply fit_transfrom to scale selected feature  
+dataset[scaling_column] = scaler.fit_transform(dataset[scaling_column])
+#Cheking min and max value of the scaling_column
+print(dataset[scaling_column].describe().T[['min','max']])
+
+''' Data Pre-processing: Konversi String ke Numerik '''
+# Kita memiliki dua kolom yang bertipe object yang dinyatakan dalam tipe data str, yaitu kolom 'Month' dan 'VisitorType'.
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+# Convert feature/column 'Month'
+LE = LabelEncoder()
+dataset['Month'] = LE.fit_transform(dataset['Month'])
+print(LE.classes_)
+print(np.sort(dataset['Month'].unique()))
+print('')
+# Convert feature/column 'VisitorType'
+LE = LabelEncoder()
+dataset['VisitorType'] = LE.fit_transform(dataset['VisitorType'])
+print(LE.classes_)
+print(np.sort(dataset['VisitorType'].unique()))
+# LabelEncoder akan mengurutkan label secara otomatis secara alfabetik lalu mengkonversi pandas objek ke numeris menurut posisi/indeks dari setiap label.
+# Dengan demikian kita telah membuat dataset menjadi bernilai numeris seluruhnya yang siap digunakan untuk pemodelan dengan algoritma machine learning.
+
