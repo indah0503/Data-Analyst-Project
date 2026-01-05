ab18dafc8e9041ed3af98f558f53714bc5ec4825
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-05 07:49:13 +0700

    Update mini-project.py

--- machine-learning/mini-project.py
+++ machine-learning/mini-project.py
@@ -91,3 +91,18 @@ print("Evaluasi Model Performance:")
 print("Training Accuracy :", logreg.score(X_train, y_train))
 print("Testing Accuracy :", logreg.score(X_test, y_test))
 
+# Import library
+from sklearn.metrics import confusion_matrix, classification_report
+
+#7. Print Confusion matrix dan classification report
+print("\n[7] Print Confusion matrix dan classification report")
+
+#apply confusion_matrix function to y_test and y_pred
+print("Confusion matrix:")
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+#apply classification_report function to y_test and y_pred
+print("Classification report:")
+cr = classification_report(y_test, y_pred)
+print(cr)
