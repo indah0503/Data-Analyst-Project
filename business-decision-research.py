3df919fbd1dc52ff9b9665ae40dc721109c1034a
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-07 14:59:04 +0700

    Update business-decision-research.py

--- business-decision-research.py
+++ business-decision-research.py
@@ -285,3 +285,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 print('Accuracy :', accuracy_score(y_test, y_pred))
 print('Precision:', precision_score(y_test, y_pred, average='micro'))
 print('Recall   :', recall_score(y_test, y_pred, average='micro'))
+# Output:
+Accuracy : 0.66668
+Precision: 0.66668
+Recall   : 0.66668
