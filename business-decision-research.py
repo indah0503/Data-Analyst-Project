9b6fcea2d698a1a6824f4de516860cc5313a669c
Indah Sekar Ningrum <91112026+indah0503@users.noreply.github.com>
2026-01-08 12:02:42 +0700

    Update business-decision-research.py

--- business-decision-research.py
+++ business-decision-research.py
@@ -256,6 +256,7 @@ logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 cnf_matrix = confusion_matrix(y_test, y_pred)
 print('Confusion Matrix:\n', cnf_matrix)
+
 # Output:
 # Confusion Matrix:
 #  [[    1  8330]
@@ -285,7 +286,8 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 print('Accuracy :', accuracy_score(y_test, y_pred))
 print('Precision:', precision_score(y_test, y_pred, average='micro'))
 print('Recall   :', recall_score(y_test, y_pred, average='micro'))
+
 # Output:
-Accuracy : 0.66668
-Precision: 0.66668
-Recall   : 0.66668
+# Accuracy : 0.66668
+# Precision: 0.66668
+# Recall   : 0.66668
